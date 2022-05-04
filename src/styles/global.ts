c69893abbf22be79ebf836203ffdd5fb0602170e
pasly0920 <inwoo920621@gmail.com>
2022-05-04 10:46:09 +0900

    [Feat] Install Global Style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,7 +6,9 @@ const GlobalStyle = createGlobalStyle`
     #root{
         background : radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
         font-family: 'Barlow Semi Condensed';
-    
+    }
+    a{
+        text-decoration : none;
     }
 `;
 
