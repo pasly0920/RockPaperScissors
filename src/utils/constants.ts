bbe8a8bf40045bd35ebf865d75e151164d2d40a4
pasly0920 <inwoo920621@gmail.com>
2022-05-06 12:23:47 +0900

    [Style] Add Style Constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,35 @@
+enum ColorCode {
+  DARK = 'hsl(229, 25%, 31%)',
+  SCORE = 'hsl(229, 64%, 46%)',
+  OUTLINE = 'hsl(217, 16%, 45%)',
+  SCISSORS1 = 'hsl(39, 89%, 49%)',
+  SCISSORS2 = 'hsl(40, 84%, 53%)',
+  PAPER1 = 'hsl(230, 89%, 62%)',
+  PAPER2 = 'hsl(230, 89%, 65%)',
+  ROCK1 = 'hsl(349, 71%, 52%)',
+  ROCK2 = 'hsl(349, 70%, 56%)',
+  LIZARD1 = 'hsl(261, 73%, 60%)',
+  LIZARD2 = 'hsl(261, 72%, 63%)',
+  CYAN1 = 'hsl(189, 59%, 53%)',
+  CYAN2 = 'hsl(189, 58%, 57%)',
+  BACKGROUND1 = 'hsl(214, 47%, 23%)',
+  BACKGROUND2 = 'hsl(237, 49%, 15%)',
+}
+
+enum GamePickCode {
+  'DEFAULT' = 0,
+  'ROCK' = 1,
+  'PAPER' = 2,
+  'SCISSOR' = 3,
+  'LIZARD' = 4,
+  'SPOCK' = 5,
+}
+
+enum FontSize {
+  LARGE = '1.5rem',
+  MEDIUM = '1rem',
+  SMALL = '0.9rem',
+  TINY = '0.8rem',
+}
+
+export { ColorCode, GamePickCode, FontSize };
