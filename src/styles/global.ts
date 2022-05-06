6491e83c5d7c3653474c6585a44d58b371229dcc
pasly0920 <inwoo920621@gmail.com>
2022-05-06 12:36:19 +0900

    [Feat] Background gradient util Constant convert

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
+import { ColorCode } from '@utils/constants';
+
 const GlobalStyle = createGlobalStyle`
     ${reset}
     #root{
-        background : radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+        background : radial-gradient(${ColorCode.BACKGROUND1}, ${ColorCode.BACKGROUND2});
         font-family: 'Barlow Semi Condensed';
     }
     a{
