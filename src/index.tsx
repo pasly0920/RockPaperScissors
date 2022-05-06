e4593a1a0b0201614774cc055d69de7f44376c7a
pasly0920 <inwoo920621@gmail.com>
2022-05-06 12:37:11 +0900

    [Fix] Absolute Path

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GlobalStyle from './styles/global';
+import GlobalStyle from '@styles/global';
 import './index.css';
 import App from './App';
 import styled from 'styled-components';
