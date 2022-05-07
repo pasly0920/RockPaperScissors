9a8d408c0603e30ec00fff1bf69487682770a70f
pasly0920 <inwoo920621@gmail.com>
2022-05-07 15:59:28 +0900

    [Fix] Sort Importing Code

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import styled from 'styled-components';
+
 import GlobalStyle from '@styles/global';
 import './index.css';
 import App from './App';
-import styled from 'styled-components';
 
 const RootContainer = styled.div`
   width: 80%;
