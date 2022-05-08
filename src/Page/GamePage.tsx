a8ded7e3084b1fa55d4fa799650fcaf5f50924c9
pasly0920 <inwoo920621@gmail.com>
2022-05-08 10:06:36 +0900

    [Feat] Seperate Game Detail component

--- src/Page/GamePage.tsx
+++ src/Page/GamePage.tsx
@@ -1,33 +1,9 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-
-import GameResult from '@Component/GameResult';
-import GamePick from '@Component/GamePick';
-import ScoreBoard from '@Component/ScoreBoard';
-import { GamePickCode, GameMode } from '@utils/constants';
-
-const RockPaperScissors = styled.div`
-  width: 70%;
-  margin: 3% auto;
-`;
+import Game from '@Component/Game';
 
 const GamePage: React.FC = () => {
-  const [gameMode, setGameMode] = useState<number>(GameMode.RPS);
-  const [userPick, setUserpick] = useState<number>(GamePickCode.DEFAULT);
-  const [RPSScore, setRPSScore] = useState<number>(0);
-  const [RPSLSScore, setRPSLSScore] = useState<number>(0);
-
-  return (
-    <RockPaperScissors>
-      <ScoreBoard
-        gameMode={gameMode}
-        gameScore={gameMode === GameMode.RPS ? RPSScore : RPSLSScore}
-      />
-      {userPick === GamePickCode.DEFAULT ? <GamePick /> : <GameResult />}
-    </RockPaperScissors>
-  );
+  return <Game />;
 };
 
 export default GamePage;
 
-//RockPaperScissorsPage
+//RockPaperScissorsGamePage
