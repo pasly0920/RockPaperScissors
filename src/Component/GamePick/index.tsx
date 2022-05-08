c8e91e200541c2b60d806965e48c5f49816f72a9
pasly0920 <inwoo920621@gmail.com>
2022-05-08 10:05:52 +0900

    [Rename] Change Game Folder directory

--- src/Component/GamePick/index.tsx
+++
@@ -1,5 +0,0 @@
-const GamePick: React.FC = () => {
-  return <div></div>;
-};
-
-export default GamePick;
--- src/Component/GameResult/index.tsx
+++
@@ -1,5 +0,0 @@
-const GameResult: React.FC = () => {
-  return <div></div>;
-};
-
-export default GameResult;
