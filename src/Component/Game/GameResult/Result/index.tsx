d53779c51df87136b89cc398487f0cd3d111d522
pasly0920 <inwoo920621@gmail.com>
2022-06-12 13:24:15 +0900

    [Feat] Result Text Initail v

---
+++ src/Component/Game/GameResult/Result/index.tsx
@@ -0,0 +1,11 @@
+import { GameResultCode } from '@utils/constants';
+
+interface Props {
+  result: number;
+}
+
+const Result: React.FC<Props> = ({ result }) => {
+  return <div></div>;
+};
+
+export default Result;
