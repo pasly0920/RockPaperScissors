4f046c36d5cd77db1df8afd13666baa553016e1d
pasly0920 <inwoo920621@gmail.com>
2022-06-12 13:23:50 +0900

    [Feat] Game Result

--- src/Component/Game/GameResult/index.tsx
+++ src/Component/Game/GameResult/index.tsx
@@ -1,19 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import PickBox from '@Component/Game/PickBox';
+import Result from './Result';
+import { GameResultContainer } from './style';
+import { GameResultCode } from '@utils/constants';
+import getResult from '@utils/getResult';
+
 interface Props {
   userPick: number;
   housePick: number;
-  resetUserPick: () => void;
-  setRPSScore: React.Dispatch<React.SetStateAction<number>>;
-  setRPSLSScore: React.Dispatch<React.SetStateAction<number>>;
+  gameMode: number;
+  resetPick: () => void;
 }
 
 const GameResult: React.FC<Props> = ({
   userPick,
   housePick,
-  resetUserPick,
-  setRPSScore,
-  setRPSLSScore,
+  gameMode,
+  resetPick,
 }) => {
-  return <div></div>;
+  const [result, setResult] = useState<number>(GameResultCode.DEFAULT);
+
+  useEffect(() => {
+    setResult(getResult(userPick, housePick, gameMode));
+  }, []);
+
+  return (
+    <GameResultContainer>
+      <PickBox pick={userPick} setUserPick={null} />
+      {result === GameResultCode.DEFAULT ? '' : <Result result={result} />}
+      <PickBox pick={housePick} setUserPick={null} />
+    </GameResultContainer>
+  );
 };
 
 export default GameResult;
---
+++ src/Component/Game/GameResult/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const GameResultContainer = styled.div`
+  margin-top: 3%;
+  margin-left: -15%;
+  width: 130%;
+  height: 70%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
