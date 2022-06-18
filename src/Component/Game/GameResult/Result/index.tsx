d96872490803062faa0059bba466cc055f558a16
pasly0920 <inwoo920621@gmail.com>
2022-06-18 20:02:14 +0900

    [Feat] Result and Again Btn

--- src/Component/Game/GameResult/Result/index.tsx
+++ src/Component/Game/GameResult/Result/index.tsx
@@ -1,11 +1,49 @@
-import { GameResultCode } from '@utils/constants';
+import { GameResultCode } from "@utils/constants";
+import {
+  ResultTextContainer,
+  ResultText,
+  AgainContainer,
+  AgainText,
+} from "./style";
 
 interface Props {
   result: number;
+  resetPick: () => void;
 }
 
-const Result: React.FC<Props> = ({ result }) => {
-  return <div></div>;
+const Result: React.FC<Props> = ({ result, resetPick }) => {
+  console.log(result);
+  switch (result) {
+    case GameResultCode.DRAW:
+      return (
+        <ResultTextContainer>
+          <ResultText>Draw</ResultText>
+          <AgainContainer onClick={resetPick}>
+            <AgainText>Play Again</AgainText>
+          </AgainContainer>
+        </ResultTextContainer>
+      );
+    case GameResultCode.LOSE:
+      return (
+        <ResultTextContainer>
+          <ResultText>You Lose</ResultText>
+          <AgainContainer onClick={resetPick}>
+            <AgainText>Play Again</AgainText>
+          </AgainContainer>
+        </ResultTextContainer>
+      );
+    case GameResultCode.WIN:
+      return (
+        <ResultTextContainer>
+          <ResultText>You Win</ResultText>
+          <AgainContainer onClick={resetPick}>
+            <AgainText>Play Again</AgainText>
+          </AgainContainer>
+        </ResultTextContainer>
+      );
+    default:
+      return <div>default</div>;
+  }
 };
 
 export default Result;
--- src/Component/Game/GameResult/Result/style.ts
+++ src/Component/Game/GameResult/Result/style.ts
@@ -1,1 +1,36 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+import { FontSize, ColorCode } from "@utils/constants";
+
+export const ResultTextContainer = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ResultText = styled.span`
+  font-size: ${FontSize.LARGE};
+  color: ${ColorCode.WHITE};
+`;
+
+export const AgainContainer = styled.div`
+  margin-top: 10%;
+  width: 80%;
+
+  border-radius: 10px;
+  background-color: ${ColorCode.WHITE};
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const AgainText = styled.span`
+  font-size: ${FontSize.MEDIUM};
+  color: ${ColorCode.BACKGROUND2};
+  margin-top: 5%;
+  margin-bottom: 5%;
+`;
